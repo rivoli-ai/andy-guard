dae3b13426a280802cc63985aeaa2dd6863efb2a
Thomas Andre <zetomatoz@gmail.com>
2025-09-03 16:29:17 -0700

    chore: fix up solution problems

--- src/Andy.Guard/InputScanners/ScanResult.cs
+++ src/Andy.Guard/InputScanners/ScanResult.cs
@@ -7,6 +7,6 @@ public class ScanResult
     public bool IsInjectionDetected { get; set; }
     public float ConfidenceScore { get; set; }
     public RiskLevel RiskLevel { get; set; }
-    public Dictionary<string, object> Metadata { get; set; }
+    public Dictionary<string, object>? Metadata { get; set; }
     public TimeSpan ProcessingTime { get; set; }
 }
